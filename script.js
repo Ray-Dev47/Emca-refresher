6f22330eb9d029af31a9efaf498104c9edd74c7a
Raymond Idengeli <ray.idengeli@gmail.com>
2022-06-11 13:35:51 +0100

    additional fixes

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // challenge 5 - ask a via prompt, check if eligible to enter at 18+ , show a msg to allow or deny depending on the age using tenary operator, setup a membership boolean value and add second check to ensure the user is a member and output a message to them if they are or not allowing or denying entry.
 
-// let ageCheck = prompt('Please enter your age to confirm eleigibility');
+let ageCheck = prompt('Please enter your age to confirm eleigibility');
 
-// if(ageCheck >= 18){
-//     alert(`You are eligible`)
-// } else{
-//     alert(`You are not eligible`)
-// }
+if(ageCheck >= 18){
+    alert(`You are eligible`)
+} else{
+    alert(`You are not eligible`)
+}
 
 // 2.)
 let ageCheck2  = 18;
