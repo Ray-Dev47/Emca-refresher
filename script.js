b8e57c83e61525605e0d8afa3cfb983c004db279
Raymond Idengeli <ray.idengeli@gmail.com>
2022-06-11 13:46:57 +0100

    age req task completed

--- script.js
+++ script.js
@@ -78,3 +78,23 @@ if(numCheck  % 2 == 0){
 
 // multiple conditionals
 
+// challenge 5 - ask a via prompt, check if eligible to enter at 18+ , show a msg to allow or deny depending on the age using tenary operator, setup a membership boolean value and add second check to ensure the user is a member and output a message to them if they are or not allowing or denying entry.
+
+let ageCheck = prompt('Please enter your age to confirm eleigibility');
+
+if(ageCheck >= 18){
+    alert(`You are eligible`)
+} else{
+    alert(`You are not eligible`)
+}
+
+// 2.)
+let ageCheck2  = 18;
+let checkAns = (ageCheck2 >= 18) ? console.log('Yes qualified') : console.log('Not qualified')
+console.log(checkAns)
+
+if(ageCheck2 >= 18){
+    console.log(`${true}, welcome`)
+}else{
+    console.log(`${false}, not qualified`)
+}
