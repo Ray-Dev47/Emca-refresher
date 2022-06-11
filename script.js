c83606d9cf9ccf28413966374ac7fae475b79bbd
Raymond Idengeli <ray.idengeli@gmail.com>
2022-06-11 18:27:59 +0100

    function init

--- script.js
+++ script.js
@@ -98,3 +98,20 @@ if(ageCheck2 >= 18){
 }else{
     console.log(`${false}, not qualified`)
 }
+
+// challenge 5 - set a default time , create a messag to be output according to the time e.g: good morning, output the message in the console.
+
+// let defaultTime = 24;
+let whatisTime = prompt('What is the time?')
+
+if(whatisTime < 11){
+    console.log(`Good Morning`)
+} else if (whatisTime >= 12 && whatisTime < 18 ){
+    console.log(`Good afternoon`)
+} else if(whatisTime >= 18 && whatisTime <= 24){
+    console.log(`Good Evening`)
+} else{
+    console.log(`Invalid Time entered`)
+}
+
+// functions - blocks of reusable codes thats performs a specific task or calculate a value
