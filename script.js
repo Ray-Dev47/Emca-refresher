251a522b4dd8d4b00202be23af528753852ad835
Raymond Idengeli <ray.idengeli@gmail.com>
2022-06-11 14:41:24 +0100

    bug fixes and cleanup

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ let whatisTime = prompt('What is the time?')
 
 if(whatisTime < 11){
     console.log(`Good Morning`)
-} else if (whatisTime >= 12 && whatisTime <= 18 ){
+} else if (whatisTime >= 12 && whatisTime < 18 ){
     console.log(`Good afternoon`)
-} else{
+} else if(whatisTime >= 18 && whatisTime <= 24){
     console.log(`Good Evening`)
+} else{
+    console.log(`Invalid Time entered`)
 }
