6fa7ebd251391f1e6b6d1e2af3969aec4583bc4f
Raymond Idengeli <ray.idengeli@gmail.com>
2022-06-20 23:45:49 +0100

    recursive function task addition

--- script.js
+++ script.js
@@ -171,5 +171,30 @@ testScope(); // works - bcos it is globally scoped
 console.log(b); // Uncaught ReferenceError: b is not defined ----> is is bcos b is locally scoped while a is globally scoped
 
 // Function recursion - allows us create a quick loop of content, its a functions that calls itself within itself
+// challenge 8 --> Create a function that asks for a guess from the user. Use the guess value to compare to see if the user guessed correctly let them know if correct or incorrect . Bonus: Using a tenary operator to provide sugestion for the next guess when the user is wrong.
 
+let rightGuess = 17;
+
+
+// function guess(){
+//     let quest = prompt('Please guess a number between 1 to 20');
+//     if(quest == rightGuess){
+//         return  alert(`yes ${rightGuess} is the correct guess`)
+//     } 
+//     else if(quest !== rightGuess){
+//         return guess(alert("Wrong ans, please try again"));
+//     }
+    
+// }
+// guess()
+
+
+// Bonus Using Tenary Operator
+
+// function guess(){
+//     let quest = prompt('Please guess a number between 1 to 20');
+//     (quest == rightGuess) ?  alert(`yes ${rightGuess} is the correct guess`) : guess(alert("Wrong ans, please try again"))
+    
+// }
+// guess()
 
